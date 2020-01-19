8ce1c801833e1b02837f5cb18bf611e65419ab0b
KamMan <kam123ex@gmail.com>
2020-01-20 02:27:50 +0800

    test2

--- ICRM_WEB/app.py
+++ ICRM_WEB/app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
-app = Flask(__name__) # Store the main app
+# from flask import Flask, render_template
+# app = Flask(__name__) # Store the main app
 
-@app.route("/") #  "@" = (Decorator) it's object, use under function 
-def index():
-    return render_template("index.html")
+# @app.route("/") #  "@" = (Decorator) it's object, use under function 
+# def index():
+#     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return "This is test"
+# @app.route("/test")
+# def test():
+#     return "This is test"
     
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+import web
+
+urls = (
+    '/index.html'
+)
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        return 'index metmod'
