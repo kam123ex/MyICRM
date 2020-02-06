80716dba944d1cf21a09ec663c509ac8b322d892
KamMan <kam123ex@gmail.com>
2020-02-06 19:25:21 +0800

    fix

---
+++ routes/books.js
@@ -0,0 +1,110 @@
+const express = require("express")
+const router = express.Router()
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+const Book = require("../models/book")
+const Author = require("../models/author")
+const uploadPath = path.join("public", Book.coverImageBasePath)
+const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"]
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
+// Books Router
+router.get("/", async (req, res) => {
+    res.send("All Books")
+    // let query = Book.find()
+    // if(req.query.title != null && req.query.title != ""){
+    //     query = query.regex("title", new RegExp(req.query.title, "i"))
+    // }
+    // if(req.query.publishedBefore != null && req.query.publishedBefore != ""){
+    //     query = query.lte("publishDate", req.query.publishedBefore,)
+    // }
+    // if(req.query.publishedAfter != null && req.query.publishedAfter != ""){
+    //     query = query.lte("publishDate", req.query.publishedAfter,)
+    // }
+    // try {
+    //     // const books = await query.exec()
+    //     res.render("books/index", {
+    //         books: books,
+    //         searchOptions: req.query
+    //     })
+    // } catch {
+    //     res.redirect("/")
+    // }
+})
+
+// New Book Router
+router.get("/new", async (req, res) => {
+    try {
+        const author = await Author.find({})
+        const book = new Book()
+        res.render("books/new", {
+            authors: authors,
+            book: book
+        })
+    } catch {
+        res.redirect("/books")
+    }
+    // renderNewPage(res, new Book())
+})
+
+// Create Book
+router.post("/", upload.single("cover"), async (req, res) => {
+    //const fileName = req.file != null ? req.file.filename : null
+    // const book = new Book({
+    //     title: req.body.title,
+    //     author: req.body.author,
+    //     publishDate: new Date(req.body.publishDate),
+    //     pageCount: req.body.pageCount,
+    //     // coverImageName: fileName,
+    //     description: req.body.description
+    // })
+
+    // try {
+    //     console.log("newBook1")
+    //     const newBook = await book.save()
+    //     console.log("newBook2")
+    //     //res.redirect(`books/${newBook.id}`)
+    //     res.redirect(`books`)
+    // } catch {
+    //     // if(book.coverImageName != null){
+    //     //     removeBookCover(book.coverImageName)
+    //     // }
+    //     renderNewPage(res, book, true)
+    // }
+})
+
+// function removeBookCover(fileName){
+//     fs.unlink(path.join(uploadPath, fileName), err => {
+//         if(err) console.error(err)
+//     })
+// }
+
+async function renderNewPage(res, book, hasError = false) {
+    try{
+        const authors = await Author.find({})
+        const params = {
+            authors: authors,
+            book: book
+        }
+
+        // console.log(
+        // params.title+
+        // params.author+
+        // params.publishDate+
+        // params.pageCount+
+        // params.coverImageName+
+        // params.description+
+        // params.book)
+        if (hasError) params.errorMessage = "Error Creating Book"
+        res.render("books/new", params)
+    }catch{
+        res.redirect("/books")
+    }
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 
 const indexRouter = require('./routes/index')
 const authorRouter = require("./routes/authors")
-
+const bookRouter = require("./routes/books")
 // var cookieParser = require("cookie-parser");
 // var util = require("util");
 // var fs = require('fs');
@@ -22,7 +22,9 @@ app.set("layout", "layouts/layout")
 
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true })
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
 const db = mongoose.connection
 db.on("error", error => console.error(error))
 db.once("open", () => console.log("Connected to Mongoose"))
@@ -33,6 +35,7 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: false}))
 
 app.use("/", indexRouter)
 app.use("/authors", authorRouter)
+app.use("/books", bookRouter)
 // app.get("/", (req, res) => {
 //   res.render("index.ejs", { name: "kam" })
 // })
